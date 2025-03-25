7e320175a1f6e3b72982371aa43bd65a4bf964f8
Mateo Garcia Carreno <mgarciac03@outlook.com>
2025-03-25 16:22:14 -0500

    CRUD operations with API system

--- api/views.py
+++ api/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics 
-from .serializers import ToDoSerializer 
-from todo.models import ToDo 
+from rest_framework import generics, permissions
+from .serializers import ToDoSerializer
+from todo.models import ToDo
 
 # Create your views here.
 
-class ToDoListCreate(generics.ListCreateAPIView): 
+class ToDoListCreate(generics.ListCreateAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and 
     # queryset. 
     # We specify TodoSerializer which we have earlier implemented 
-    serializer_class = ToDoSerializer 
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self): 
-        user = self.request.user 
+    def get_queryset(self):
+        user = self.request.user
         return ToDo.objects.filter(user=user).order_by('-created')
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class ToDoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # user can only update, delete own posts 
+        return ToDo.objects.filter(user=user)
